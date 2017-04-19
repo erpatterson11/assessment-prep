c00bd37faca9e1177e6ebfaab68d1342161387c5
Eric Patterson <Eric Patterson>
2017-04-18 18:25:26 -0600

    completed basic-js-review

--- basic-js-review/review.js
+++ basic-js-review/review.js
@@ -1,33 +1,43 @@
 /////////////Variables/////////////
 //1. Write a variable called color with a value of "dark blue".
 
+var color = "dark blue";
+
 //2. Write a variable, myFave, that has the value "My favorite color is "
+
+var myFave = "My favorite color is";
 
 //3. Use the variables from 1 and 2 to make myFavoriteColor equal
 //   "My favorite color is dark blue" (concatenate)
-var myFavoriteColor;
+var myFavoriteColor = myFave + " " + color;
 
 //4. Create an array of strings that includes the letters a - d in order.
-var aThroughD;
-//5. Remove 'a' and store it to a variable called isForApple.
-
-//6. Add 'e' to the end of aThroughD.
-
-//7. Add 'apple' to the beginning of aThroughD
-
-//8. Remove 'c' from the array and put 'cat' in its place
-
-//9. Leave 'apple' in the array but put a copy of it on a var called bestFruit
-
-//10. Create an object called spaceJam
-//    spaceJam has: a title of 'Space Jam' (String)
-//                  a release_year of 1996 (Number)
-//                  is not an oscarAwardWinner (Boolean)
-//                  and has the stars Michael Jordan, Bugs Bunny, and Bill Murray (array of strings)
-
-
-//11. Change anakin's side to dark using bracket notation
-
+var aThroughD = ["a","b","c","d"];
+// //5. Remove 'a' and store it to a variable called isForApple.
+var isForApple = aThroughD.shift();
+// //6. Add 'e' to the end of aThroughD.
+aThroughD.push("e");
+// //7. Add 'apple' to the beginning of aThroughD
+aThroughD.unshift("apple")
+// //8. Remove 'c' from the array and put 'cat' in its place
+aThroughD[2] = "cat"
+// //9. Leave 'apple' in the array but put a copy of it on a var called bestFruit
+var bestFruit = aThroughD[0];
+// //10. Create an object called spaceJam
+// //    spaceJam has: a title of 'Space Jam' (String)
+// //                  a release_year of 1996 (Number)
+// //                  is not an oscarAwardWinner (Boolean)
+// //                  and has the stars Michael Jordan, Bugs Bunny, and Bill Murray (array of strings)
+//
+var spaceJam = {
+  title: "Space Jam",
+  release_year: 1996,
+  oscarAwardWinner: false,
+  stars: ["Michael Jordan", "Bugs Bunny", "Bill Murray"]
+}
+//
+// //11. Change anakin's side to dark using bracket notation
+//
 var anakinSkywalker =  {
   side: 'light',
   other_names: ['Darth Vader', 'Ani'],
@@ -36,31 +46,64 @@ var anakinSkywalker =  {
   former_mother: 'Shmi Skywalker'
 }
 
-//12. Add 'Jar Jar' to his children using dot notation
+anakinSkywalker.side = "dark";
+//
+// //12. Add 'Jar Jar' to his children using dot notation
 
-//13. Loop through the object and remove any property that starts with former
+anakinSkywalker["children"].push("Jar Jar");
 
+// //13. Loop through the object and remove any property that starts with former
 
+for (var keys in anakinSkywalker) {
+  if (keys.indexOf("former") > -1) {
+    delete anakinSkywalker[keys];
+  }
+}
 
-//14. Create a function called finder that takes in an array and a string and loops through the array
-//    to remove every instance of that string
-
-
+// //14. Create a function called finder that takes in an array and a string and loops through the array
+// //    to remove every instance of that string
+//
+function finder(arr,str) {
+  for (var i in arr) {
+    if (arr[i] === str) {
+      arr.splice(i,1);
+    }
+  }
+}
 
 //15. Write a function called numChecker that takes in a parameter and checks to see if it is a number or a
   //number in a string (ie '12' or 12) - if so, return true, else return false
 
+function numChecker(par) {
+  var num = parseInt(par);
+  if (num) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 
 //16. Write a function called rightNow that returns a new date object
 
-
+function rightNow() {
+  return new Date();
+}
 
 //17. Write a function called whoAmI that takes in an object and returns the value of the
 //    object's name property, and if the object's name
 //    is undefined, return 'has amnesia'.
 
 
+function whoAmI(obj) {
+  return (obj.name ? obj.name : "has amnesia");
+}
+
+
 
 //18. Write a function called returnMe that takes in a num and a callback function and
 //    returns the result of the callback function invoked with the num as a parameter
+
+function returnMe(num,callback) {
+  return callback(num);
+}
